860ff00c2fc71ccc25f1272326a8429bc2ed4b7d
Chaitanya Kharya <Chaitanya Kharya@DESKTOP-U64IEN0>
2022-09-06 08:55:36 +0530

    Bubble Sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+	
+	static void bubbleSort(int[] arr) {  
+		int n = arr.length;  
+		int temp = 0;  
+		for(int i=0; i < n; i++){  
+			for(int j=1; j < (n-i); j++){  
+				if(arr[j-1] > arr[j]){  
+					temp = arr[j-1];  
+					arr[j-1] = arr[j];  
+		            arr[j] = temp;  
+		        }  
+		                          
+		    }  
+		
+		}  
+	
+	}  
+		    
+	public static void main(String[] args) {  
+		        
+		Scanner sc = new Scanner(System.in);            	
+		System.out.println("Enter size of array:");
+		int size = sc.nextInt();   
+		
+		
+		System.out.println("Enter array:");
+		int[] arr = new int[size];
+		for(int i=0; i < arr.length; i++){  
+			arr[i] = sc.nextInt();  
+		}
+		bubbleSort(arr);
+        System.out.println("Array a4fter bubble sort");  
+		for(int i=0; i < arr.length; i++){  
+			System.out.print(arr[i] + " ");  
+		}  
+		   
+	}
+		    
+}
+
+
+
